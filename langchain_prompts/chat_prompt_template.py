74c702b265d45e1321f558bc13488df3dfecb6ec
priyansudash1999 <1999priyansudash@gmail.com>
2025-11-25 07:57:06 +0530

    dynamic chat for multi turn conversation

---
+++ langchain_prompts/chat_prompt_template.py
@@ -0,0 +1,14 @@
+from langchain_core.prompts import ChatPromptTemplate
+
+chat_template = ChatPromptTemplate([
+  ('system', 'You are a helpful {domain} expert'),
+  ('human', "Explain in simple terms, what is {topic}")
+])
+
+
+prompt = chat_template.invoke({
+  "domain": "coding",
+  "topic": "React"
+})
+
+print(prompt)
